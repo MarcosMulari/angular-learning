a02bc360b0d930288de2ca42cc098ee2411b9ce5
Marcos <mvrmulari@gmail.com>
2025-08-21 22:30:20 -0300

    angular-material toolbar; css global e temas; inicio de cadastro

--- crud-angular-material/src/app/app.ts
+++ crud-angular-material/src/app/app.ts
@@ -1,12 +1,23 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+  imports: [
+    RouterLink,
+    RouterOutlet,
+    MatTableModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule
+  ],
+  templateUrl: './app.html'
 })
-export class App {
-  protected readonly title = signal('crud-angular-material');
-}
+export class App {}
+
